10f325e8fefc212ef096cae017d83e97ae09c341
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-04-12 00:23:48 +0200

    Add files via upload

---
+++ easy_calc/main.js
@@ -0,0 +1,7 @@
+function sumValues() {
+    var num1, num2, oper, res;
+    num1=Number(document.formcalc.txtnum1.value);
+    num2=Number(document.formcalc.txtnum2.value);
+    res = num1 + num2;
+    document.formcalc.txtres.value=res;
+};
