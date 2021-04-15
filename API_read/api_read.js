266853dfb448d13b5a87aaebbc64712df62a5b42
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-04-15 15:47:27 +0200

    Update api_read.js

--- API_read/api_read.js
+++ API_read/api_read.js
@@ -8,7 +8,7 @@ request.onload = function () {
   if (request.status >= 200 && request.status < 400) {
     data.forEach((movie) => {
       document.write(movie.title);
-      document.write("<br>");
+      document.write("<br>")
     })
   } else {
     console.log('error')
