8bd16cadb4650b21faf24287b7387ba9bced151b
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-04-15 15:25:27 +0200

    Update api_read.js

--- API_read/api_read.js
+++ API_read/api_read.js
@@ -1,6 +1,17 @@
-const request = new XMLHttpRequest();
-request.open("GET", "https://jsonplaceholder.typicode.com/users");
-request.send();
-request.onload = ()=>{
-    console.log(request);
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((movie) => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
+  }
 }
+
+request.send()
