aa5e4d6f1854e99538414d7b0772f01d964067e0
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-04-20 14:43:34 +0200

    Update api_test.js

--- API_test/api_test.js
+++ API_test/api_test.js
@@ -1,16 +1,3 @@
-var request = new XMLHttpRequest()
-
-document.write("
-                     <table class="w3-table-all">
-                      <tr>
-                        <th>Title</th>
-                        <th>Rate</th>
-                       </tr>
-                      <tr>");
-
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-
 fetch('https://ghibliapi.herokuapp.com/films')
   .then(response => {
     return response.json();
