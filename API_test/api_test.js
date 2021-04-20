11f41b65274eff89deed3edea5a88c0ac1abbaa2
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-04-20 13:50:35 +0200

    Update api_test.js

--- API_test/api_test.js
+++ API_test/api_test.js
@@ -1,6 +1,21 @@
-const request = new XMLHttpRequest();
-request.open("GET", "https://jsonplaceholder.typicode.com/users");
-request.send();
-request.onload = ()=>{
-    console.log(request);
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach((movie) => {
+      document.write("Title: ");
+      document.write(movie.title);
+      document.write(", Rate: ");
+      document.write(movie.rt_score);
+      document.write("%,<br>")
+    })
+  } else {
+    console.log('error')
+  }
 }
+
+request.send()
