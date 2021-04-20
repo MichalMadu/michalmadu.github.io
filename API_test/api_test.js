22965fd1097250e3e43e90fc7bceff0c984588bb
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-04-20 14:25:29 +0200

    Update api_test.js

--- API_test/api_test.js
+++ API_test/api_test.js
@@ -1,5 +1,13 @@
 var request = new XMLHttpRequest()
 
+document.write("
+                     <table class="w3-table-all">
+                      <tr>
+                        <th>Title</th>
+                        <th>Rate</th>
+                       </tr>
+                      <tr>");
+
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function () {
   // Begin accessing JSON data here
@@ -7,13 +15,7 @@ request.onload = function () {
 
   if (request.status >= 200 && request.status < 400) {
     data.forEach((movie) => {
-      document.write("
-                     <table class="w3-table-all">
-                      <tr>
-                        <th>Title</th>
-                        <th>Rate</th>
-                       </tr>
-                      <tr>");
+      
                      
       document.write("Title: ");
       document.write(movie.title);
@@ -27,3 +29,6 @@ request.onload = function () {
 }
 
 request.send()
+
+document.write("
+                     </table>");
