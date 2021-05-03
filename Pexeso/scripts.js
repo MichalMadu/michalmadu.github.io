3cb7f8a8033ee86bc17e0896510e7758af19a66d
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-05-03 12:17:33 +0200

    Update scripts.js

--- Pexeso/scripts.js
+++ Pexeso/scripts.js
@@ -1,1 +1,7 @@
-var
+const cards = document.querySelectorAll('.memory-card');
+
+function flipCard() {
+  console.log('I was clicked!');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
