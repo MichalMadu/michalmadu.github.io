63ac302fcdb61775707ad2434361acd1c8ab7759
MichalMadu <79706745+MichalMadu@users.noreply.github.com>
2021-05-05 01:05:47 +0200

    final commit 5.5.

--- Pexeso/scripts.js
+++ Pexeso/scripts.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.toggle('flip');
   
   if (!hasFlippedCard) {
@@ -12,7 +16,7 @@ function flipCard() {
     firstCard = this; 
    } else {
      // second card
-     hasFlippedCard = false;
+     
      secondCard = this;
      
    checkForMatch();
@@ -21,24 +25,46 @@ function flipCard() {
 
 function checkForMatch() {
     // Do cards match?
-     if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        disableCards();      
-     } else {
-       //not a match
-        unflipCards();     
-     }
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+// I've used ternaty operator instead of if else block
+// condition ? true : false
+isMatch ? disableCards() : unflipCards();
+// if (isMatch) {
+//        disableCards();      
+//    } else {
+//      //not a match
+//        unflipCards();     
+//   }
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
-      secondCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
+  lockBoard = true;
+
    setTimeout(() => {
        firstCard.classList.remove('flip');
        secondCard.classList.remove('flip');
-       }, 1500);
+  
+      resetBoard();
+      }, 1500);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null]
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let = randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
